023e07ee2474eb34817811dfda62817ead69c00a
PunnawithSut <punnawith05@gmail.com>
2026-07-14 21:44:24 +0700

    fix: offline capabilities of serwist

--- kosen-app/src/app/sw.ts
+++ kosen-app/src/app/sw.ts
@@ -13,11 +13,22 @@ declare global {
 declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST,
+  precacheEntries: [
+    ...(self.__SW_MANIFEST || []),
+    { url: "/offline", revision: "1" },
+  ],
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
+  fallbacks: {
+    entries: [
+      {
+        url: "/offline",
+        matcher: ({ request }) => request.destination === "document",
+      },
+    ],
+  },
 });
 
 serwist.addEventListeners();
